4f94be2d548e0a43e4dd42abdfd284136728d05b
IsaacDavidS <isaacdsolisb@gmail.com>
2021-10-18 20:39:57 -0500

    cambios principales

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const PUERTO = 3030
+const app = express();
+const path = require('path')
+
+const publicPath = path.resolve(__dirname, './public')
+
+app.use(express.static(publicPath));
+
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/login', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.get('/register', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+
+
+app.listen(PUERTO, () => console.log('Corriendo en 3030'));
